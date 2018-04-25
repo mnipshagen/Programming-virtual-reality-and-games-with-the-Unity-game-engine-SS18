635c36ebe18e4339dbfac080b51cf1bab4bff288
Falk Heuer <faheuer@uos.de>
2018-04-25 16:36:34 +0200

    added pickups

--- Assets/Collide.cs
+++ Assets/Collide.cs
@@ -18,7 +18,12 @@ public class Collide : Axe {
 
     }
 
-    void OnTriggerEnter(Collider c) {
+    void OnTriggerEnter(Collider c) 
+	{
+		if (c.gameObject.CompareTag("Pick up"))
+		{
+			c.gameObject.SetActive (false);
+		}
         GameObject other = c.gameObject;
         IAxeHittable target = other.GetComponent<IAxeHittable>();
         Debug.Log("We hit something");
