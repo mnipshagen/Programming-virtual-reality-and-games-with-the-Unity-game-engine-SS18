cbb4990cfdc00abec44ef95e547d4dfec2b07aa7
Kristin Schmidt <krschmidt@uos.de>
2018-04-25 14:52:36 +0200

    add chest, UI and different fire particle effect to labyrinth

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour {
+public class UIController : MonoBehaviour {
 
-    public GameState gameState;
-    //public GUIText text;
     public Text text;
     public Canvas canvas;
     public int startMessageDuration = 4;
     public Color fadeColor = new Color(0, 0, 1, 2.0f/255.0f);
     private long startTime;
+    private bool fadingOutText;
 
 	// Use this for initialization
 	void Start () {
-        //canvas.GetComponentInChildren<Text>;
         fadeColor = new Color(0, 0, 0, 2.0f / 255.0f);
+        fadingOutText = true;
     }
 
     // Update is called once per frame
@@ -26,16 +25,19 @@ public class GameController : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        //Debug.Log(text.color);
-        //Debug.Log(Time.fixedTime);
-        if(Time.fixedTime > startMessageDuration && text.color.a > 0)
+        if(Time.fixedTime > startMessageDuration && fadingOutText)
         {
             text.color -= fadeColor;
+            if(text.color.a <= 0)
+            {
+                fadingOutText = false;
+            }
         }
-        if(Time.fixedTime > 7)
-        {
-            text.text = "You found the treasure, \n you win!";
-            text.color += new Color(0, 0, 0, 1);
-        }
+    }
+
+    public void ShowWinMessage()
+    {
+        text.text = "You found the treasure, \n you win!";
+        text.color += new Color(0, 0, 0, 1);
     }
 }
